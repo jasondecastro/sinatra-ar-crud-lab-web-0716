b728769b89e333bc6902efe686e2261e3fe1da3f
Jason Decastro <jasonrdecastro@gmail.com>
2016-08-14 19:43:55 -0400

    finished sinatra ar crud lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,54 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+    erb :index 
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :posts
+  end
+
+  get '/posts/:id' do
+    if params[:id].include?("new")
+      erb :new_post
+    elsif Post.exists?(params[:id]) == false
+      erb 'sorry there, bud<br>why not <a href="/recipes/new">create a new post?</a>'
+    else
+      @post = Post.find(params[:id])
+      erb :post
+    end
+  end
+
+  get '/posts/new' do
+    erb :'/new_post'
+  end
+
+  post '/create_post' do
+    Post.create(name: params[:name], content: params[:content])
+    redirect("/posts")
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit_post
+  end
+
+  patch '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    @post.update(name: params[:name], content: params[:content])
+    redirect("/posts/#{@post.id}")
+  end
+
+  get '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    erb :delete_post
+  end
+
+  delete '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    @post.destroy
+    erb "#{@post.name} was deleted"
   end
 end
